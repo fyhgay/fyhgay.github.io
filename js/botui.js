52b3fa2e10764aaf1dc14ff61347a4f822dde4fa
Magneto <2240325136@qq.com>
2020-05-04 19:13:11 +0800

    Site updated: 2020-05-04 19:13:09

--- js/botui.js
+++ js/botui.js
@@ -26,11 +26,11 @@ function bot_ui_ini() {
     }).then(function () {
         botui.message.add({
             delay: 1100,
-            content: "这里是 Mashiro"
+            content: "这里是 Magneto"
         }).then(function () {
             botui.message.add({
                 delay: 1100,
-                content: "一个可爱的蓝孩子~"
+                content: "一个可爱的蓝孩子~同时也是一个同性恋..."
             }).then(function () {
                 botui.action.button({
                     delay: 1600,
@@ -64,33 +64,33 @@ function bot_ui_ini() {
         },
         secondpart = function () {
             botui.message.add({
-                delay: 1500,
-                content: "目前就读于上海财经大学"
+                delay: 2000,
+                content: "曾经我愧于说出我是同性恋的真相……"
             }).then(function () {
                 botui.message.add({
                     delay: 1500,
-                    content: "向往技术却误入商科，但后来喜欢上了经济学…"
+                    content: "直到他来到我的世界里"
                 }).then(function () {
                     botui.message.add({
                         delay: 1200,
-                        content: "因为数据分析也需要Coder嘛"
+                        content: "我才敢于说出来"
                     }).then(function () {
                         botui.message.add({
-                            delay: 1500,
-                            content: "主攻 R 语言和 Python，略懂 STATA，偶尔也折腾 HTML/CSS/JavaScript/PHP"
+                            delay: 1300,
+                            content: "虽然他后来离开了我"
                         }).then(function () {
                             botui.message.add({
-                                delay: 1500,
-                                content: "研究的方向，是经济/金融方向的数据分析（data science）以及机器学习（machine learning）"
+                                delay: 2000,
+                                content: "但是我认为我应该坚强起来，努力学习！"
                             }).then(function () {
                                 botui.message.add({
-                                    delay: 1800,
-                                    content: "喜欢画画，希望有一天能够被称为画师"
+                                    delay: 1200,
+                                    content: "期待未来的新篇章！"
                                 }).then(function () {
                                     botui.action.button({
                                         delay: 1100,
                                         action: [{
-                                            text: "为什么叫Mashiro呢？ 🤔",
+                                            text: "为什么叫Magneto呢？ 🤔",
                                             value: "why-mashiro"
                                         }]
                                     }).then(function (a) {
@@ -106,12 +106,12 @@ function bot_ui_ini() {
         thirdpart = function () {
             botui.message.add({
                 delay: 1E3,
-                content: "Mashiro以及站名都来自一部动画，因为和主角有一样的爱好~ 如果有兴趣可以找找首页上的视频~"
+                content: "~我是一个漫威粉，但又不想做大多数~~"
             }).then(function () {
                 botui.action.button({
                     delay: 1500,
                     action: [{
-                        text: "为什么是白猫呢？ 🤔",
+                        text: "为什么是Don't Cry呢？ 🤔",
                         value: "why-cat"
                     }]
                 }).then(function (a) {
@@ -122,11 +122,11 @@ function bot_ui_ini() {
         fourthpart = function () {
             botui.message.add({
                 delay: 1E3,
-                content: "因为对GitHub有种执念… "
+                content: "我曾经为了一个男孩子哭过… "
             }).then(function () {
                 botui.message.add({
-                    delay: 1100,
-                    content: "而且我真的是猫控！"
+                    delay: 1500,
+                    content: "我想我现在应该坚强起来！"
                 }).then(function () {
                     botui.action.button({
                         delay: 1500,
@@ -143,7 +143,7 @@ function bot_ui_ini() {
         fifthpart = function () {
             botui.message.add({
                 delay: 1E3,
-                content: "emmmm，看备案信息你就知道了=.= 本来想要zheng.xin的，但50万真买不起。。"
+                content: "I'm a Gay 直接表达出来~"
             }).then(function () {
                 botui.message.add({
                     delay: 1600,
